0f20003ad071899935c0032809744e254d220912
Joe Jones <jjones@wallickcos.com>
2011-03-20 00:36:13 -0400

    Added initial command line parser.  Need to finish.

--- DList2XML/Program.cs
+++ DList2XML/Program.cs
@@ -8,20 +8,64 @@ namespace DList2XML
     {
         static void Main(string[] args)
         {
-            if (args.Length == 1)
-                DisplayHelp(true);
-            Console.ReadKey();
+            try
+            {
+                string InputDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                string OutputFile = "dlist.xml";
+                string[] ExcludedDirectories = new string[0];
+                bool Recursive = false;
+
+                if (args.Length == 0)
+                    DisplayHelp(true);
+
+                foreach (string arg in args)
+                {
+                    string myArg = arg.Substring(0, 2);
+                    switch (myArg)
+                    {
+                        case "-i":
+                            string[] aInput = arg.Split('=');
+
+                            break;
+
+                        case "-o":
+                            string[] aOutput = arg.Split('=');
+
+                            break;
+
+                        case "-e":
+                            string[] aExclude = arg.Split('=');
+
+                            break;
+                        case "-r":
+                            Recursive = true;
+                            break;
+                    }
+                }
+
+
+
+                Console.WriteLine(string.Format(" Input directory is:    {0}", InputDirectory));
+                Console.WriteLine(string.Format(" Output file is:        {0}", OutputFile));
+                Console.WriteLine(string.Format(" Excluded directory is: {0}", ExcludedDirectories.ToString()));
+                Console.WriteLine(string.Format(" Recursive Search:      {0}", Recursive.ToString()));
+            }
+            catch (Exception ex) { Console.WriteLine(string.Format("EXCEPTION: {0}", ex.Message)); }
         }
 
         private static void DisplayHelp(bool bExit)
         {
             if (bExit == true)
             {
+                Console.Clear();
+                Console.WriteLine("");
                 Console.WriteLine("DList2XML arguments");
                 Console.WriteLine("  -i, input directory");
                 Console.WriteLine("  -o, output file");
                 Console.WriteLine("  -e, excluded directory");
                 Console.WriteLine("  -r, recrusive");
+                Console.WriteLine("");
+                Console.WriteLine("");
                 Console.Write("Press any key to exit...");
                 Console.ReadKey();
                 Environment.Exit(1);
