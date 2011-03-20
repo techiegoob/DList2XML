0b778fadd9b8bfc1f33ba826ca1c505976b1f9e2
Joe Jones <jjones@wallickcos.com>
2011-03-19 23:20:38 -0400

    initial commit

---
+++ DList2XML/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DList2XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+                DisplayHelp(true);
+            Console.ReadKey();
+        }
+
+        private static void DisplayHelp(bool bExit)
+        {
+            if (bExit == true)
+            {
+                Console.WriteLine("DList2XML arguments");
+                Console.WriteLine("  -i, input directory");
+                Console.WriteLine("  -o, output file");
+                Console.WriteLine("  -e, excluded directory");
+                Console.WriteLine("  -r, recrusive");
+                Console.Write("Press any key to exit...");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+        }
+    }
+}
