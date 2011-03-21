86d466a60949806bbcae2146f46d5e4018b95a7d
Joe Jones <jjones@wallickcos.com>
2011-03-21 09:44:19 -0400

    - fixed output file
    - defaults to run silently, added verbose option.

--- DList2XML/Program.cs
+++ DList2XML/Program.cs
@@ -11,9 +11,10 @@ namespace DList2XML
             try
             {
                 string InputDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                string OutputFile = "dlist.xml";
+                string OutputFile = InputDirectory+@"\dlist.xml";
                 string[] ExcludedDirectories = new string[0];
                 bool Recursive = false;
+                bool Verbose = false;
 
                 if (args.Length == 0)
                     DisplayHelp(true);
@@ -40,15 +41,21 @@ namespace DList2XML
                         case "-r":
                             Recursive = true;
                             break;
+                        case "-v":
+                            Verbose = true;
+                            break;
                     }
                 }
 
 
-
-                Console.WriteLine(string.Format(" Input directory is:    {0}", InputDirectory));
-                Console.WriteLine(string.Format(" Output file is:        {0}", OutputFile));
-                Console.WriteLine(string.Format(" Excluded directory is: {0}", ExcludedDirectories.ToString()));
-                Console.WriteLine(string.Format(" Recursive Search:      {0}", Recursive.ToString()));
+                if (Verbose == true)
+                {
+                    Console.WriteLine(string.Format(" Input directory is:    {0}", InputDirectory));
+                    Console.WriteLine(string.Format(" Output file is:        {0}", OutputFile));
+                    Console.WriteLine(string.Format(" Excluded directory is: {0}", ExcludedDirectories.ToString()));
+                    Console.WriteLine(string.Format(" Recursive Search:      {0}", Recursive.ToString()));
+                    Console.WriteLine(string.Format(" Verbose:               {0}", Verbose.ToString()));
+                }
             }
             catch (Exception ex) { Console.WriteLine(string.Format("EXCEPTION: {0}", ex.Message)); }
         }
@@ -64,6 +71,7 @@ namespace DList2XML
                 Console.WriteLine("  -o, output file");
                 Console.WriteLine("  -e, excluded directory");
                 Console.WriteLine("  -r, recrusive");
+                Console.WriteLine("  -v, verbose, print out what is happening");
                 Console.WriteLine("");
                 Console.WriteLine("");
                 Console.Write("Press any key to exit...");
